acbf40f460a1e8fd00fd7bc4d635fc7346f23d73
lumicsoft <lumicsoft@gmail.com>
2026-02-24 17:17:43 +0530

    Update web3-handler.js

--- web3-handler.js
+++ web3-handler.js
@@ -28,7 +28,7 @@ const CONTRACT_ABI = [
     "function getLevelTeamDetails(address _upline, uint256 _level) view returns (string[] names, address[] wallets, uint256[] joinDates, uint256[] activeDeps, uint256[] teamTotalDeps, uint256[] teamActiveDeps, uint256[] withdrawals)",
     "function getLiveBalance(address uA) view returns (uint256 pendingROI)",
     "function users(address) view returns (address referrer, string username, bool registered, uint256 joinDate, uint256 totalActiveDeposit, uint256 teamActiveDeposit, uint256 teamTotalDeposit, uint256 totalDeposited, uint256 totalWithdrawn, uint256 totalEarnings)",
-    "function usersExtra(address) view returns (uint256 rewardsReferral, uint256 rewardsRank, uint256 reserveDailyROI, uint32 teamCount, uint32 directsCount, uint32 directsQuali, uint8 rank)",
+    "function usersExtra(address) view returns (uint256 rewardsReferral, uint256 rewardsRank, uint256 reserveDailyROI,uint256 totalEarnedROI,uint256 totalEarnedLevel,uint256 totalEarnedRank,  uint32 teamCount, uint32 directsCount, uint32 directsQuali, uint8 rank)",
     "function getPosition(address uA, uint256 i) view returns (tuple(uint256 amount, uint256 startTime, uint256 lastCheckpoint, uint256 endTime, uint256 earned, uint256 expectedTotalEarn, bool active) v)",
     "function getUserTotalPositions(address uA) view returns (uint256)",
     "function getUserHistory(address _user) view returns (tuple(string txType, uint256 amount, uint256 timestamp, string detail)[])"
@@ -734,6 +734,7 @@ function updateNavbar(addr) {
 }
 
 window.addEventListener('load', init);
+
 
 
 
